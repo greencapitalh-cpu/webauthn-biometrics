9a94bf630e0cc4d0936393596eeaa5bed440bffe
greencapitalh-cpu <greencapitalh@gmail.com>
2025-11-13 14:45:23 -0400

    Update mongo.js

--- db/mongo.js
+++ db/mongo.js
@@ -1,19 +1,17 @@
 import { MongoClient } from "mongodb";
 
-let db, credentials;
-
+let db, users;
 export async function connectDB() {
   try {
     const client = new MongoClient(process.env.MONGODB_URI);
     await client.connect();
-    db = client.db("webauthn");
-    credentials = db.collection("credentials");
+    db = client.db("udochain");
+    users = db.collection("bioid_users");
     console.log("✅ Connected to MongoDB");
   } catch (err) {
-    console.error("❌ MongoDB connection error:", err.message);
+    console.error("❌ MongoDB error:", err.message);
   }
 }
-
-export function getCredentials() {
-  return credentials;
+export function getUsers() {
+  return users;
 }
