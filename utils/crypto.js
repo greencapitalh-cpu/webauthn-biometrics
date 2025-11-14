a4c06653913423487b9a1e5f8fc863a76710089b
greencapitalh-cpu <greencapitalh@gmail.com>
2025-11-13 20:43:12 -0400

    Update crypto.js

--- utils/crypto.js
+++ utils/crypto.js
@@ -1,7 +1,2 @@
 import crypto from "crypto";
-
-export const sha256Hex = (input) =>
-  crypto.createHash("sha256").update(input).digest("hex");
-
-export const randomBase64 = (size = 32) =>
-  crypto.randomBytes(size).toString("base64url");
+export const sha256Hex = (str) => crypto.createHash("sha256").update(str).digest("hex");
