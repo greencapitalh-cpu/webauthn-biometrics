154c2f8bb56c333619c633cb1736128cf24913d4
greencapitalh-cpu <greencapitalh@gmail.com>
2025-11-15 01:58:52 -0400

    Update bioidRoutes.js

--- routes/bioidRoutes.js
+++ routes/bioidRoutes.js
@@ -1,5 +1,13 @@
 import express from "express";
-import { startEnroll, finishEnroll, startVerify, finishVerify, checkStatus } from "../controllers/bioidController.js";
+import {
+  startEnroll,
+  finishEnroll,
+  startVerify,
+  finishVerify,
+  checkStatus,
+  getUserByHash, // nuevo controlador
+} from "../controllers/bioidController.js";
+
 const router = express.Router();
 
 router.get("/status/:userId", checkStatus);
@@ -7,5 +15,8 @@ router.post("/enroll/start", startEnroll);
 router.post("/enroll/finish", finishEnroll);
 router.post("/verify/start", startVerify);
 router.post("/verify/finish", finishVerify);
+
+// 🆕 Buscar datos por bioidHash
+router.get("/hash/:hash", getUserByHash);
 
 export default router;
